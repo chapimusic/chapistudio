e3945e2fc32885dcc8f6b87cf924d817dbe02912
Joris <joris@saidani.org>
2020-07-27 21:42:39 +0200

    add socials

--- schemas/documents/siteSettings.js
+++ schemas/documents/siteSettings.js
@@ -1,51 +1,69 @@
 export default {
-    name: 'siteSettings',
-    type: 'document',
-    title: 'Paramètres',
-    __experimental_actions: [
-        //'create',
-        'update',
-        //'delete',
-        'publish'
-    ],
-    fields: [
-        {
-            name: 'title',
-            type: 'string',
-            title: 'Title'
-        },
-        {
-            name: 'url',
-            type: 'string',
-            title: 'URL'
-        },
-        {
-            name: 'description',
-            type: 'text',
-            title: 'Description',
-            description: 'Describe your website for search engines and social media.'
-        },
-        {
-            name: 'keywords',
-            type: 'array',
-            title: 'Keywords',
-            description: 'Add keywords that describes your portfolio.',
-            of: [{type: 'string'}],
-            options: {
-                layout: 'tags'
-            }
-        },
-        {
-            name: 'instagram',
-            type: 'string',
-            title: 'Instagram',
-            description: 'username'
-        },
-        {
-            name: 'facebook',
-            type: 'string',
-            title: 'Facebook',
-            description: 'username'
-        }
-    ]
-}
+  name: "siteSettings",
+  type: "document",
+  title: "Paramètres",
+  __experimental_actions: [
+    //'create',
+    "update",
+    //'delete',
+    "publish",
+  ],
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      name: "url",
+      type: "string",
+      title: "URL",
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Description",
+      description: "Describe your website for search engines and social media.",
+    },
+    {
+      name: "keywords",
+      type: "array",
+      title: "Keywords",
+      description: "Add keywords that describes your portfolio.",
+      of: [{ type: "string" }],
+      options: {
+        layout: "tags",
+      },
+    },
+    {
+      name: "instagram",
+      type: "string",
+      title: "Instagram",
+      description: "username",
+    },
+    {
+      name: "facebook",
+      type: "string",
+      title: "Facebook",
+      description: "username",
+    },
+    {
+      name: "youtube",
+      type: "string",
+      title: "Facebook",
+      description: "username",
+    },
+    {
+      name: "twitter",
+      type: "string",
+      title: "Facebook",
+      description: "username",
+    },
+    {
+      name: "spotify",
+      type: "string",
+      title: "Facebook",
+      description: "username",
+    },
+  ],
+};
