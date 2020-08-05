4e9aaff6bb0260e9977e1a03a8114162af8c129f
Joris <joris@saidani.org>
2020-08-05 13:11:14 +0200

    video description required

--- schemas/objects/video.js
+++ schemas/objects/video.js
@@ -1,17 +1,18 @@
 export default {
-    name: 'video',
-    type: 'object',
-    title: 'Video Embed',
-    fields: [
-        {
-            name: 'url',
-            type: 'url',
-            title: 'URL YouTube de la vidéo'
-        },
-        {
-            name: 'description',
-            type: 'string',
-            title: 'Description',
-        }
-    ]
-}
+  name: "video",
+  type: "object",
+  title: "Video Embed",
+  fields: [
+    {
+      name: "url",
+      type: "url",
+      title: "URL YouTube de la vidéo",
+    },
+    {
+      name: "description",
+      type: "string",
+      title: "Description",
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+};
